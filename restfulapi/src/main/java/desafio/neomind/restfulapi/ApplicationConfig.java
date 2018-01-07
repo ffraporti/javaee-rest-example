468e93f28a2058b49ca1c3557dd5b060f649421b
Felipe Fraporti <ffraporti@gmail.com>
2018-01-07 20:08:35 -0200

    * Removed servlet-mapping from web.xml.
    * Created Application Config Class to be used as entry point.

---
+++ restfulapi/src/main/java/desafio/neomind/restfulapi/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package desafio.neomind.restfulapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class ApplicationConfig extends Application{
+
+}
--- restfulapi/src/main/java/desafio/neomind/restfulapi/Provider.java
+++ restfulapi/src/main/java/desafio/neomind/restfulapi/Provider.java
@@ -5,13 +5,16 @@ import java.util.Date;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
- 
+
+
 @Path("provider")
 public class Provider{
+	
 	@GET
-	@Produces("text/html")
-	public Response getStartingPage()
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getProvider()
 	{
 		String output = "<h1>Hello World!<h1>" +
 				"<p>RESTful Service is running ... <br>Ping @ " + new Date().toString() + "</p<br>";
