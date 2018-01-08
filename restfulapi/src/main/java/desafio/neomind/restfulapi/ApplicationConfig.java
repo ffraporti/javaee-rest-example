968ba476534be99ff2b26f7070324590f220ef19
business <business@neo>
2018-01-08 14:24:39 -0200

    Removed old Project

--- restfulapi/src/main/java/desafio/neomind/restfulapi/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package desafio.neomind.restfulapi;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("v1")
-public class ApplicationConfig extends Application{
-
-}
